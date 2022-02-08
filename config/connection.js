292143e0663ed354b97e89136393c420654e5f21
samuel lutz <samlutz89@gmail.com>
2022-02-07 20:32:09 -0800

    time out error on server

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,8 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: 'root',
+      dialect: 'mysql'
     }
   );
 }
